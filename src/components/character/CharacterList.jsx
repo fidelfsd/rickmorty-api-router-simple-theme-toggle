945fadf95a583956f696e55c741d3ad52fc5cf95
Fidel <f.gilart@geekshubs.com>
2024-06-20 17:17:53 +0200

    Add useCharacterList custom hook

--- src/components/character/CharacterList.jsx
+++ src/components/character/CharacterList.jsx
@@ -1,42 +1,18 @@
-// src/components/CharacterList/CharacterList.js
-import { useEffect, useState } from "react";
-import rickMortyService from "../../services/rickMorty";
 import Loader from "../common/Loader";
 import CharacterCard from "./CharacterCard";
 import "../../styles/components/CharacterList.css";
+import { useCharacterList } from "../../hooks/useCharacterList";
 
 export default function CharacterList() {
-   const [characters, setCharacters] = useState([]);
-   const [loading, setLoading] = useState(true);
-   const [error, setError] = useState(null);
-   const [page, setPage] = useState(1); // Estado para la página actual
-   const [totalPages, setTotalPages] = useState(0); // Estado para el número total de páginas
-
-   useEffect(() => {
-      const fetchCharacters = async () => {
-         setLoading(true); // Mostrar el cargador al cambiar de página
-         try {
-            const data = await rickMortyService.characters.getAll(page); // Pasar la página al servicio
-            setCharacters(data.results);
-            setTotalPages(data.info.pages); // Establecer el número total de páginas
-         } catch (err) {
-            setError(err.message);
-         } finally {
-            setLoading(false);
-         }
-      };
-
-      fetchCharacters();
-   }, [page]); // Agregar `page` como dependencia para recargar al cambiar de página
-
-   // Manejar la navegación de la página
-   const handlePrevPage = () => {
-      if (page > 1) setPage(page - 1);
-   };
-
-   const handleNextPage = () => {
-      if (page < totalPages) setPage(page + 1);
-   };
+   const {
+      characters,
+      error,
+      page,
+      totalPages,
+      loading,
+      handlePrevPage,
+      handleNextPage,
+   } = useCharacterList();
 
    if (loading) return <Loader />;
    if (error) return <p>Error: {error}</p>;
---
+++ src/hooks/useCharacterList.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import rickMortyService from "../services/rickMorty";
+
+export const useCharacterList = () => {
+   const [characters, setCharacters] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+   const [page, setPage] = useState(1);
+   const [totalPages, setTotalPages] = useState(0);
+
+   useEffect(() => {
+      const fetchCharacters = async () => {
+         setLoading(true);
+         try {
+            const data = await rickMortyService.characters.getAll(page);
+            setCharacters(data.results);
+            setTotalPages(data.info.pages);
+         } catch (err) {
+            setError(err.message);
+         } finally {
+            setLoading(false);
+         }
+      };
+
+      fetchCharacters();
+   }, [page]);
+
+   const handlePrevPage = () => {
+      if (page > 1) setPage(page - 1);
+   };
+
+   const handleNextPage = () => {
+      if (page < totalPages) setPage(page + 1);
+   };
+
+   return {
+      characters,
+      page,
+      totalPages,
+      error,
+      loading,
+      handleNextPage,
+      handlePrevPage,
+   };
+};
