9d44623b88578596e6352fdbf2ed89cd4cc2eb47
Fidel <f.gilart@geekshubs.com>
2024-06-22 22:16:40 +0200

    refactor(Header): use absolute import for style

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "../../styles/layout/Header.css";
+import "@styles/layout/Header.css";
 import { useTheme } from "@hooks/useTheme";
 
 export default function Header() {
